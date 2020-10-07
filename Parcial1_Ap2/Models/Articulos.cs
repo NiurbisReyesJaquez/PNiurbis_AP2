dd9d715f6d80aee034e805ace748becb915714d7
NiurbisReyesJaquez <71194774+NiurbisReyesJaquez@users.noreply.github.com>
2020-10-07 18:09:25 -0400

    Models

---
+++ Parcial1_Ap2/Models/Articulos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        [Required (ErrorMessage = "En este campo es obligatorio introducir la descripcion del articulo")]
+        public string Descripcion { get; set; }
+        [Range (minimum:1, maximum:10000, ErrorMessage ="Su rango es de 1 a 10000")]
+        public double Existencia { get; set; }
+        [Range(minimum:1, maximum:1000000, ErrorMessage ="Su rango es de 1 a 1000000")]
+        public double Costo { get; set; }
+        public double ValorInventario { get; set; }
+    }
+}
